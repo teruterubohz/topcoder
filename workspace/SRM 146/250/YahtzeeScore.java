b58e59f65799088fb415982b5c9d77cb95b03b83
teruterubohz <teruterubohz@gmail.com>
2018-02-08 00:12:37 +0900

    SRM146 250

---
+++ workspace/SRM 146/250/YahtzeeScore.java
@@ -0,0 +1,106 @@
+import java.io.*;
+import java.util.*;
+
+public class YahtzeeScore {
+	public int maxPoints(int[] toss) {
+		int array[] = {0,0,0,0,0,0};
+
+		for(int i = 0 ; i < toss.length ; i++){
+			array[toss[i]-1] += toss[i];
+		}
+
+		int max = 0;
+		for( int i = 0 ; i < array.length ; i++)
+			if( max < array[i])
+				max = array[i];
+
+		return max;
+	}
+
+// CUT begin
+	public static void main(String[] args){
+		System.err.println("YahtzeeScore (250 Points)");
+		System.err.println();
+		HashSet<Integer> cases = new HashSet<Integer>();
+        for (int i = 0; i < args.length; ++i) cases.add(Integer.parseInt(args[i]));
+        runTest(cases);
+	}
+
+	static void runTest(HashSet<Integer> caseSet) {
+	    int cases = 0, passed = 0;
+	    while (true) {
+	    	String label = Reader.nextLine();
+	    	if (label == null || !label.startsWith("--"))
+	    		break;
+
+            int[] toss = new int[Integer.parseInt(Reader.nextLine())];
+            for (int i = 0; i < toss.length; ++i)
+                toss[i] = Integer.parseInt(Reader.nextLine());
+            Reader.nextLine();
+            int __answer = Integer.parseInt(Reader.nextLine());
+
+            cases++;
+            if (caseSet.size() > 0 && !caseSet.contains(cases - 1))
+                continue;
+    		System.err.print(String.format("  Testcase #%d ... ", cases - 1));
+
+            if (doTest(toss, __answer))
+                passed++;
+	    }
+	    if (caseSet.size() > 0) cases = caseSet.size();
+        System.err.println(String.format("%nPassed : %d/%d cases", passed, cases));
+
+        int T = (int)(System.currentTimeMillis() / 1000) - 1518003704;
+        double PT = T / 60.0, TT = 75.0;
+        System.err.println(String.format("Time   : %d minutes %d secs%nScore  : %.2f points", T / 60, T % 60, 250 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT))));
+	}
+
+	static boolean doTest(int[] toss, int __expected) {
+		long startTime = System.currentTimeMillis();
+		Throwable exception = null;
+		YahtzeeScore instance = new YahtzeeScore();
+		int __result = 0;
+		try {
+			__result = instance.maxPoints(toss);
+		}
+		catch (Throwable e) { exception = e; }
+		double elapsed = (System.currentTimeMillis() - startTime) / 1000.0;
+
+		if (exception != null) {
+			System.err.println("RUNTIME ERROR!");
+			exception.printStackTrace();
+			return false;
+		}
+		else if (__result == __expected) {
+			System.err.println("PASSED! " + String.format("(%.2f seconds)", elapsed));
+			return true;
+		}
+		else {
+			System.err.println("FAILED! " + String.format("(%.2f seconds)", elapsed));
+			System.err.println("           Expected: " + __expected);
+			System.err.println("           Received: " + __result);
+			return false;
+		}
+	}
+
+	static class Reader {
+        private static final String dataFileName = "YahtzeeScore.sample";
+	    private static BufferedReader reader;
+
+	    public static String nextLine() {
+	        try {
+                if (reader == null) {
+                    reader = new BufferedReader(new InputStreamReader(new FileInputStream(dataFileName)));
+                }
+                return reader.readLine();
+	        }
+	        catch (IOException e) {
+	            System.err.println("FATAL!! IOException");
+	            e.printStackTrace();
+	            System.exit(1);
+	        }
+	        return "";
+	    }
+	}
+// CUT end
+}
